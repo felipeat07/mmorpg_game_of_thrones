efcfcb092e3c8ad3b4ae4db00b867ec733bcb14e
Felipe Tavares <felipeat07@gmail.com>
2022-04-20 16:41:27 +0100

    recebendo e validando dados na tela cadastro e instalando modulo mongodb

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,23 @@
 module.exports.cadastro = function(application, req, res){
-    res.render('cadastro.ejs');
+    res.render('cadastro.ejs', {validacao: {}, dadosForm: {}});
+}
+
+module.exports.cadastrar = function(application, req, res){
+    
+    var dadosForm = req.body;
+
+    req.assert('nome', 'O campo Nome não pode estar vazio').notEmpty();
+    req.assert('usuario', 'O campo Usuário não pode estar vazio').notEmpty();
+    req.assert('senha', 'O campo Senha não pode estar vazio').notEmpty();
+    req.assert('casa', 'O campo Casa não pode estar vazio').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('cadastro', {validacao: errors, dadosForm: dadosForm})
+        return;
+    }
+
+        res.send('podemos continuar com o cadastro');
+
 }
