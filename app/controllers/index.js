66099e216bf0cf827fd6ae917a0fd3715b21a71d
Felipe Tavares <felipeat07@gmail.com>
2022-04-21 11:09:38 +0100

    criando o controller de autenticação

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,22 @@
 module.exports.index = function(application, req, res){
-	res.render('index');
+	res.render('index', {validacao: {}});
+}
+
+module.exports.autenticar = function(application, req, res){
+	
+	var dadosForm = req.body;
+
+	req.assert('usuario', 'Usuário não deve ser vazio').notEmpty();
+	req.assert('senha', 'Senha não deve ser vazio').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render("index", {validacao: errors});
+		console.log(errors);
+		return;
+	}
+
+	res.send('teste, ir para autenticação');
+
 }
