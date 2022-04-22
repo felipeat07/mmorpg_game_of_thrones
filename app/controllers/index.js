427e6b50e9d62234d95b997c99312543db6001a0
Felipe Tavares <felipeat07@gmail.com>
2022-04-22 11:38:17 +0100

    autenticando ususario com express-session

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -17,6 +17,11 @@ module.exports.autenticar = function(application, req, res){
 		return;
 	}
 
-	res.send('teste, ir para autenticação');
+	var connection = application.config.dbConnection;
+	var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+
+	UsuariosDAO.autenticar(dadosForm, req, res);
+
+	//res.send('teste, ir para autenticação');
 
 }
